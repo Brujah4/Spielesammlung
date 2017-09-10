194a40797194c17304de0c64e9eaaa34b8515317
Bob <Bob@10.158.47.80>
2017-09-10 21:19:13 +0200

    main verschoben

--- VierGewinnt/src/backend/VierGewinnt.java
+++ VierGewinnt/src/backend/VierGewinnt.java
@@ -1,9 +1,19 @@
 package backend;
 
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import frontend.Hauptfenster;
+
 public class VierGewinnt {
 
-	public static void main(String[] args) {
-		System.out.println("go!");
-	}
+	public static void main(String[] args) 
+    {
+        Hauptfenster frame=new Hauptfenster();
+        frame.setSize(new Dimension(449, 410));
+        frame.setTitle("Vier Gewinnt");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
 
 }
